1542ea2173de04f22648e7965533e55447959c52
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-04-23 18:08:49 -0300

    feat: carousel system added in testimonials

--- main.js
+++ main.js
@@ -30,3 +30,13 @@ window.addEventListener('scroll', function () {
     header.classList.remove('scroll')
   }
 })
+
+/* Testimonials slider */
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
