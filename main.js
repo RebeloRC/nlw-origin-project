a7467fc25ac706fa34fdca6dfab5266487589910
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-04-26 17:01:01 -0300

    feat: min 1200px media querie added

--- main.js
+++ main.js
@@ -20,16 +20,16 @@ for (const link of links) {
 
 /* Sombra do header com movimento do scroll*/
 
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+function headerShadow() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function () {
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
     header.classList.remove('scroll')
   }
-})
+}
 
 /* Testimonials slider */
 const swiper = new Swiper('.swiper', {
@@ -38,7 +38,13 @@ const swiper = new Swiper('.swiper', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    1200: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 
 /* scrollreveal */
@@ -55,18 +61,27 @@ scrollReveal.reveal(
    #about .image, #about .text,
    #services header, #services .card,
    #testimonials header, #testimonials .testimonials,
-   #contact .text, #contact .links
+   #contact .text, #contact .links,
+   footer .brand, footer .social
   `,
   { interval: 100 }
 )
 
 /* animação no botão back to top */
 
-const backToTopBtn = document.querySelector('.back-to-top')
-window.addEventListener('scroll', function () {
+function backToTop() {
+  const backToTopBtn = document.querySelector('.back-to-top')
+
   if (window.scrollY >= 560) {
     backToTopBtn.classList.add('show')
   } else {
     backToTopBtn.classList.remove('show')
   }
+}
+
+/* when scroll */
+
+window.addEventListener('scroll', function () {
+  headerShadow()
+  backToTop()
 })
