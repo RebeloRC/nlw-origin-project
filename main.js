1f6610803269f90f430127ba76b54fcf25e0e4fe
Rodrigo Rebelo <rrebelo553@gmail.com>
2022-04-26 19:26:16 -0300

    style: menu animation added

--- main.js
+++ main.js
@@ -19,11 +19,10 @@ for (const link of links) {
 }
 
 /* Sombra do header com movimento do scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
 function headerShadow() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
-
   if (window.scrollY >= navHeight) {
     header.classList.add('scroll')
   } else {
@@ -68,10 +67,9 @@ scrollReveal.reveal(
 )
 
 /* animação no botão back to top */
+const backToTopBtn = document.querySelector('.back-to-top')
 
 function backToTop() {
-  const backToTopBtn = document.querySelector('.back-to-top')
-
   if (window.scrollY >= 560) {
     backToTopBtn.classList.add('show')
   } else {
@@ -79,9 +77,34 @@ function backToTop() {
   }
 }
 
+/* menu selector */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
 /* when scroll */
 
 window.addEventListener('scroll', function () {
   headerShadow()
   backToTop()
+  activateMenuAtCurrentSection()
 })
